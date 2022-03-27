64afcddb680ddf2543e7eac9c6ce869ceb39b9f5
mominur-cloud <83661855+mominur-cloud@users.noreply.github.com>
2022-03-27 14:28:02 +1100

    tfc-test

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
+variable "secret" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+
+
+
+
+
